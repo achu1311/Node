c6cff44f03586501d8deb92f9fed3a28131ff71f
R <671976@cognizant.com>
2018-03-02 09:38:05 +0530

    friday morning

--- Weather/app.js
+++ Weather/app.js
@@ -1,26 +1,47 @@
-const req=require('request');
 const yargs=require('yargs');
+const geocode=require('./geocode.js');
+const weather=require('./weather.js');
+
 const argv= yargs.options({
   a:{
     demand:true,
     alias:'address',
     describe:'Address to fetch',
     string:true
-  }
+   }//,b:{
+//     demand:true,
+//     alias:'add',
+//     describe:'fetch',
+//     string:true
+//   }
 })
 .help()
 .alias('help','h')
 .argv;
-console.log(argv.a);
-var val=encodeURIComponent(argv.a);
+//console.log(argv.a);
 
-var address='https://maps.googleapis.com/maps/api/geocode/json?address='+val;
-console.log(address);
-req({url:'https://maps.googleapis.com/maps/api/geocode/json?address=chinnamanur',json:true},(error,response,body)=>{
-//          console.log(JSON.stringify(body,undefined,1));
-console.log(body.results[0].geometry.location.lat);
-  console.log(body.results[0].geometry.location.lng);
-
+geocode.geocodeAddress(argv.a,(errMes,results)=>{
+  if(errMes){
+    console.log(errMes); }
+  else {
+    console.log(JSON.stringify(results,undefined,2))
+  }
+//   console.log(results.longitude+" "+results.latitude);
+ weather.weatherTemp(results.latitude,results.longitude,function(err,res){
+  if(err){
+    console.log(err);
+  }
+  else {
+    console.log(typeof res);
+    
+    console.log(res);
+  
+    console.log(JSON.stringify(res,undefined,2));
+  }
+  
+  
+});
+ 
 });
 
 
