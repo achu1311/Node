6f304bdff7318f60a6f370ff3d00bd967fbf9318
R <671976@cognizant.com>
2018-03-02 14:47:03 +0530

    afternoon

--- Mongo/PostMan/server.js
+++ Mongo/PostMan/server.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const mongoose=require('mongoose');
 const bodyparser=require('body-parser');
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.Promise=global.Promise;
 const {todo}=require('./Todo');
 var app=express();
 app.use(bodyparser.json());
@@ -9,6 +11,34 @@ app.post('/todos',(req,res)=>{
 var toDo=new todo({
     text:req.body.text
 });
+
+
+
+//updating values to mongo
+
+// app.post('/todos',(req,res)=>{
+
+// var toDo=new todo({  
+//     text:req.body.text
+// });
+
+// toDo.save().then((doc)=>{
+//     res.send(doc);
+
+
+// console.log(doc);
+// }
+// ,(err)=>{
+
+// res.status(400).send();
+// });
+
+// console.log(JSON.stringify(req.body));
+
+// res.send(req.body);
+//})
+
+//
 
 toDo.save().then((doc)=>{res.send(doc);}
 ,(err)=>{
@@ -21,24 +51,4 @@ res.status(400).send();
 app.listen(3000,()=>{
 
     console.log("ss");
-});
-
-// mongoose.connect('mongodb://localhost:27017/TodoApp');
-// mongoose.Promise=global.Promise;
-
-// var Todo=mongoose.model('ToDoValues',   //here ToDoValues is collection value
-// {
-// text:{type:String,required:true,trim:true},
-// completed:{type:Boolean,default:false},
-// completedAt:{type:Number,default:null}
-
-
-// });
-
-
-// var newToDo=new Todo({
-//     text:'cook dinner'
-// });
-
-
-
+})
