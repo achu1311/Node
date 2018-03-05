0c8026afefcf2cfe1fff51b3958df3879ab5e190
R <671976@cognizant.com>
2018-03-05 15:02:16 +0530

    Afternnon 3/5/18

--- Testing/utils/utils.test.js
+++ Testing/utils/utils.test.js
@@ -1,16 +1,23 @@
 const utils=require('./utils');
 const expect=require('expect');
-
-
+describe('Utils',()=>{
 it('should add ',()=>{
 var res=utils.add(33,11);
 expect(res).toBe(44).toBeA('number');
+
 // if(res==44)
 // throw new Error("Wrong value");
-});
+
+describe('Second',()=>{
+
 it('array ',()=>{
 expect({name:'achu',age:22}).toInclude({name:'achu'})
 });
+})
+
+});
+})
+
 
 it('should add2 ',(done)=>{
 utils.asyncAdd(33,11,(sum)=>{
