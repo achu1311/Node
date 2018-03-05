e468e795a236b48264f280da78c94a4718375056
R <671976@cognizant.com>
2018-03-05 09:40:13 +0530

    3/5/18 Morn

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,15 +1,13 @@
 const path=require('path');
 var add=path.join(__dirname,"../public")
-console.log(__dirname,"..","/public");
+// console.log(__dirname,"..","/public");
+var port=process.env.PORT||3000;
 console.log(add);
 const express=require('express');
 var app=express();
 app.use(express.static(add));
 
-// app.get('val',(res,req)=>{
-// res.send("Yes");
-// });
-app.listen(3000,()=>{
+app.listen(port,()=>{
 
-console.log("Listening");
+console.log(`Listening on port ${port}`);
 });
