6b1fb89431201c1494c4e1b81cc27666fc38093e
R <671976@cognizant.com>
2018-03-06 11:04:06 +0530

    3/6/18(1)

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,13 +1,15 @@
 const path=require('path');
+const express=require('express');
+var http=require('http');
+const socketIO=require('socket.io'); 
 var add=path.join(__dirname,"../public")
 // console.log(__dirname,"..","/public");
 var port=process.env.PORT||3000;
-console.log(add);
-const express=require('express');
 var app=express();
+var server=http.createServer(app);
 app.use(express.static(add));
 
-app.listen(port,()=>{
+server.listen(port,()=>{
 
 console.log(`Listening on port ${port}`);
 });
